75bbdfbae1eca47d475033644aec4d9992b16d1b
skippetr <hsker@yandex.ru>
2017-03-01 23:28:40 +0300

    fix bugs 4

--- config/db.php
+++ config/db.php
@@ -7,3 +7,11 @@ return [
     'password' => '',
     'charset' => 'utf8',
 ];
+
+//return [
+//    'class' => 'yii\db\Connection',
+//    'dsn' => 'mysql:host=127.0.0.1;dbname=rm163',
+//    'username' => 'root',
+//    'password' => '0Gk21BIAhNxyfZAg',
+//    'charset' => 'utf8',
+//];
